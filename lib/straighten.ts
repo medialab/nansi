011fb79ac6c0bb65e1719867fb5fa895ef93b6e7
Yomguithereal <guillaumeplique@gmail.com>
2020-11-18 22:01:58 +0100

    Drafting lib

---
+++ lib/straighten.ts
@@ -0,0 +1,15 @@
+import Graph from 'graphology-types';
+import seedrandom from 'seedrandom';
+
+// const RNG = seedrandom('nansi');
+
+/**
+ * Function used to "straighten" a graph by adding missing positions,
+ * for instance.
+ */
+export default function straighten(graph: Graph) {
+  // const minX: number;
+  // const maxX: number;
+  // const minY: number;
+  // const maxY: number;
+}
