5421d816ffb39d303e7864b31ef0e4f1fbd6ca35
Yomguithereal <guillaumeplique@gmail.com>
2020-11-27 14:35:25 +0100

    Straightening graph on upload

--- app/actions.ts
+++ app/actions.ts
@@ -1,4 +1,7 @@
+import Graph from 'graphology-types';
 import {useSetRecoilState} from 'recoil';
+
+import straighten from '../lib/straighten';
 
 import * as atoms from './atoms';
 
@@ -6,8 +9,12 @@ export function useSetNewGraph() {
   const setView = useSetRecoilState(atoms.view);
   const setGraph = useSetRecoilState(atoms.graph);
 
-  return graph => {
+  return (graph: Graph) => {
     setView('basemap');
+
+    // Graph processing
+    straighten(graph);
+
     setGraph(graph);
   };
 }
