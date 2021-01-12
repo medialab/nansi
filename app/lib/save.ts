d3c3835365a2bebe3f616a76dc952d2367ad0057
Yomguithereal <guillaumeplique@gmail.com>
2021-01-12 10:19:40 +0100

    saveCanvas helper

--- app/lib/save.ts
+++ app/lib/save.ts
@@ -1,9 +1,21 @@
 import {saveAs} from 'file-saver';
 
-type SaveType = 'application/xml' | 'application/json';
+type SaveTextType = 'application/xml' | 'application/json';
 
-export function saveText(name: string, text: string, type: SaveType) {
+export function saveText(name: string, text: string, type: SaveTextType) {
   const blob = new Blob([text], {type: `${type};charset=utf-8`});
 
   saveAs(blob, name);
+}
+
+type SaveCanvasType = 'image/png';
+
+export function saveCanvas(
+  name: string,
+  canvas: HTMLCanvasElement,
+  type: SaveCanvasType = 'image/png'
+) {
+  canvas.toBlob(blob => {
+    saveAs(blob, name);
+  }, type);
 }
