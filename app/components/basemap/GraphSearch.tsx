41ac6a6692dd8a8f8b84babb4ddcb0aac09f6279
Yomguithereal <guillaumeplique@gmail.com>
2021-01-19 18:06:20 +0100

    More efficient search

--- app/components/basemap/GraphSearch.tsx
+++ app/components/basemap/GraphSearch.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
+import sortBy from 'lodash/sortBy';
 
 import ThemedSelect from '../misc/ThemedSelect';
 
@@ -13,15 +14,19 @@ function GraphSearchNoOptionsMessage({inputValue}) {
 export default function GraphSearch({graph, renderer, targetAttribute}) {
   const [selectedOption, setSelectedOption] = useState(null);
 
-  const options = [];
+  const options = useMemo(() => {
+    const o = [];
 
-  graph.forEachNode((node, attr) => {
-    const label = attr[targetAttribute.name];
+    graph.forEachNode((node, attr) => {
+      const label = attr[targetAttribute.name];
 
-    if (label) {
-      options.push({value: node, label});
-    }
-  });
+      if (label) {
+        o.push({value: node, label});
+      }
+    });
+
+    return sortBy(o, 'label');
+  }, [graph, targetAttribute]);
 
   const onChange = option => {
     if (option === selectedOption) return;
